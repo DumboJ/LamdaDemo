46488510e26cf99c206f47fc80e9bb1b5d474a7a
dumboj <597824751@qq.com>
2021-09-16 23:50:05 +0800

    filter DEMO

--- expressiondmeo/src/cn/dumboj/stream/filter/Demo.java
+++ expressiondmeo/src/cn/dumboj/stream/filter/Demo.java
@@ -1,4 +1,13 @@
 package cn.dumboj.stream.filter;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Demo {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(6, 7, 3, 8, 1, 2, 9);
+        Stream<Integer> stream = list.stream();
+        stream.filter(x -> x > 7).forEach(System.out::println);
+    }
 }
