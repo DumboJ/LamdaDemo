05017ce525081dda09f94a6c266fb467b7b26337
dumboj <597824751@qq.com>
2021-09-17 23:11:58 +0800

    demo finish

--- expressiondmeo/src/cn/dumboj/stream/max_min_count/Demo.java
+++ expressiondmeo/src/cn/dumboj/stream/max_min_count/Demo.java
@@ -1,4 +1,16 @@
 package cn.dumboj.stream.max_min_count;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
 public class Demo {
+    public static void main(String[] args) {
+
+        List<String> list = Arrays.asList("adnm", "admmt", "pot", "xbangd", "weoujgsd");
+
+        Optional<String> max = list.stream().max(Comparator.comparing(String::length));
+        System.out.println("最长的字符串：" + max.get());
+    }
 }
