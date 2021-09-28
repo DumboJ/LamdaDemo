bd42a0282f0f2f18c6fab21b64cd955dd0faec9f
dumboj <597824751@qq.com>
2021-09-28 23:14:42 +0800

    to map

--- expressiondmeo/src/cn/dumboj/stream/collect/toList_toMap_toSet/Demo.java
+++ expressiondmeo/src/cn/dumboj/stream/collect/toList_toMap_toSet/Demo.java
@@ -11,11 +11,7 @@ public class Demo {
         List<Integer> listNew = list.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
         Set<Integer> set = list.stream().filter(x -> x % 2 == 0).collect(Collectors.toSet());
 
-        List<Person> personList = new ArrayList<Person>();
-        personList.add(new Person("Tom", 8900, 23, "male", "New York"));
-        personList.add(new Person("Jack", 7000, 25, "male", "Washington"));
-        personList.add(new Person("Lily", 7800, 21, "female", "Washington"));
-        personList.add(new Person("Anni", 8200, 24, "female", "New York"));
+        List<Person> personList = Person.getPerson();
 
         Map<?, Person> map = personList.stream().filter(p -> p.getSalary() > 8000)
                 .collect(Collectors.toMap(Person::getName, p -> p));
