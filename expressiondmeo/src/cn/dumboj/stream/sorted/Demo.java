341e7a647ab4ce8c9cf8dab904644128b4196b25
DumboJ <dumbojxx@gmail.com>
2022-03-05 23:41:58 +0800

    sorted demo

--- expressiondmeo/src/cn/dumboj/stream/sorted/Demo.java
+++ expressiondmeo/src/cn/dumboj/stream/sorted/Demo.java
@@ -1,4 +1,15 @@
 package cn.dumboj.stream.sorted;
 
+import cn.dumboj.stream.Person;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Demo {
+    public static void main(String [] args){
+        List<Person> person = Person.getPerson();
+        List<String> collect = person.stream().sorted(Comparator.comparing(Person::getSalary)).map(Person::getName).collect(Collectors.toList());
+        collect.stream().forEach(System.out::println);
+    }
 }
