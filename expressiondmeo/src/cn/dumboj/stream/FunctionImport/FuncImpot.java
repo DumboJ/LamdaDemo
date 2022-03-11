1e3fe986ab38b880f7e44d8f88878aae9ab10925
DumboJ <dumbojxx@gmail.com>
2022-03-12 00:01:40 +0800

    func import generate new map

--- expressiondmeo/src/cn/dumboj/stream/FunctionImport/FuncImpot.java
+++ expressiondmeo/src/cn/dumboj/stream/FunctionImport/FuncImpot.java
@@ -1,9 +1,10 @@
 package cn.dumboj.stream.FunctionImport;
 
+import cn.dumboj.stream.Person;
 import com.sun.xml.internal.bind.v2.TODO;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -15,8 +16,11 @@ public class FuncImpot {
     public static void main(String[] args) {
         //例子不太恰当，凑合用吧
         //TODO eta使用案例
+        List<Person> list = Person.getPerson();
+        LinkedHashMap<String, Person> collect1 = list.stream().collect(Collectors.toMap(Person::getName, Function.identity(), (o1, o2) -> o2, LinkedHashMap::new));
         List<String> strings = Arrays.asList("ab", "", "rr", "mm", "", "dd");
         List<Boolean> collect = strings.stream().map(x ->  x.isEmpty()).map(Boolean::new).collect(Collectors.toList());
         System.out.println(collect);
+        System.out.println(collect1);
     }
 }
